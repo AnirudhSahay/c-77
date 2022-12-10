dea2ce6a6faf8288210a5434f7021143222cd9ff
AnirudhSahay <101455843+AnirudhSahay@users.noreply.github.com>
2022-12-10 17:26:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var first_name="Harry ";
+console.log(first_name);
+var last_name="Maguire";
+console.log(last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a=27;
+console.log(a);
+var b=9;
+console.log(b);
+var add= a + b;
+console.log(add);
+var subtract= a - b;
+console.log(subtract);
+var multiply= a * b;
+console.log(multiply);
+var divide= a / b;
+console.log(divide);
